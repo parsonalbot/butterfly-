0160bfb7517f1870e8425a443a1cc0a36abdd17e
parsonalbot <thebot783@gmail.com>
2025-07-31 13:40:50 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61552257412748"];
+		const permission = ["100091401814450"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("You don't have permission to use this command.", event.threadID, event.messageID);
 		}
