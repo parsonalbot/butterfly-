07ec3cb3f29a504d300ad3ecb76775e103ca5d07
parsonalbot <thebot783@gmail.com>
2025-08-05 01:20:52 +0600

    Update shell.js

--- scripts/cmds/shell.js
+++ scripts/cmds/shell.js
@@ -17,7 +17,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, message, event }) {
-    const allowedUID = "100091401814450";
+    const allowedUID = "100055496720330";
     const senderID = event.senderID;
 
     if (senderID !== allowedUID) {
